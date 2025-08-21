42b206dbb041bb5eae5d97eec1aadde970bfb3af
Roberta Oliveira dos Santos <163215880+dossantos15@users.noreply.github.com>
2025-08-21 16:39:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ print('Loop Start')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quitting...)
             pygame.quit()
             quit()
